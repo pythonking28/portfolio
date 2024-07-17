61b022eabda86049d899f5bc5fedbcf110e38a58
pythonking28 <nirajthakur2828@gmail.com>
2024-07-17 21:24:50 +0545

    Jpg Images replaced with png images

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -6,7 +6,7 @@ const Project = () => {
             <h1 className="px-auto text-center pt-10 text-2xl sm:text-4xl lg:text-6xl font-bold">PROJECTS</h1>
             <div className="flex flex-wrap gap-10 mx-auto w-[80%] md:gap-20 p-6 items-center justify-center">
                 <div className="object-cover flex flex-col justify-center items-center gap-4 rounded-2xl hover:shadow-lg hover:shadow-[#807f7c] relative transition-all project-code">
-                    <Image src="/images/project/youtube.jpg" className="border-solid border-2 rounded-2xl" alt="html image" width={400} height={400} />
+                    <Image src="/images/project/youtube.png" className="border-solid border-2 rounded-2xl" alt="youtube" width={400} height={400} />
                     <div className="hidden button-wrapper py-2 px-5">
                         <h1 className="text-white font-medium text-xl">Youtube-Backend</h1>
                         <div className="flex gap-10">
@@ -19,7 +19,7 @@ const Project = () => {
 
                 </div>
                 <div className="object-cover flex flex-col justify-center items-center gap-4 rounded-2xl hover:shadow-lg hover:shadow-[#3FA2F6] relative transition-all project-code ">
-                    <Image src="/images/project/veg-restro.jpg" className="border-solid border-2 rounded-2xl" alt="html image" width={400} height={400} />
+                    <Image src="/images/project/veg-restro.png" className="border-solid border-2 rounded-2xl" alt="veg-restro" width={400} height={400} />
                     <div className="hidden button-wrapper py-2 px-5">
                         <h1 className="text-white font-medium text-xl">Veg-Restro</h1>
                         <div className="flex gap-10">
@@ -36,7 +36,7 @@ const Project = () => {
 
                 </div>
                 <div className="object-cover flex flex-col justify-center items-center gap-4 rounded-2xl hover:shadow-lg hover:shadow-[#FFDE4D] relative transition-all project-code">
-                    <Image src="/images/project/tasknote.jpg" className="border-solid border-2 rounded-2xl" alt="html image" width={400} height={400} />
+                    <Image src="/images/project/tasknote.png" className="border-solid border-2 rounded-2xl" alt="task note" width={400} height={400} />
                     <div className="hidden button-wrapper py-2 px-5">
                         <h1 className="text-white font-medium text-xl">Task-Note</h1>
                         <div className="flex gap-10">
